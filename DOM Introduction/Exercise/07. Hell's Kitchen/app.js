0d4532e082e508aaa4c6eb1eda85a060ab580853
Petko Todorov <p.todorov1015@gmail.com>
2023-11-25 12:50:08 +0200

    done

--- DOM Introduction/Exercise/07. Hell's Kitchen/app.js
+++ DOM Introduction/Exercise/07. Hell's Kitchen/app.js
@@ -9,7 +9,7 @@ function solve() {
         restaurantsData.forEach(element => {
 
             let [restaurant, workers] = element.split(' - ');
-            if (!restaurants.hasOwnProperty()) {
+            if (!restaurants.hasOwnProperty(restaurant)) {
                 restaurants[restaurant] = {
                     averageSalary: 0,
                     bestSalary: -1,
@@ -20,13 +20,16 @@ function solve() {
             let totalSalary = 0;
             for (const iterator of workers.split(", ")) {
                 let [name, salary] = iterator.split(" ")
-                totalSalary += Number(salary)
                 restaurants[restaurant].workers[name] = Number(salary)
                 if (Number(salary) > restaurants[restaurant].bestSalary) {
                     restaurants[restaurant].bestSalary = Number(salary);
                 }
             }
-            restaurants[restaurant].averageSalary = totalSalary / workers.split(", ").length;
+
+            Object.values(restaurants[restaurant].workers).forEach(element => {
+                totalSalary += element
+            });
+            restaurants[restaurant].averageSalary = totalSalary / Object.keys(restaurants[restaurant].workers).length; //restaurants[restaurant].workers.length;
         });
 
         let bestRestaurant = {
@@ -35,7 +38,6 @@ function solve() {
             bestSalary: -1,
             workers: {},
         };
-
         Object.entries(restaurants).forEach(element => {
             const restaurantsData = element[1];
             if (restaurantsData.averageSalary > bestRestaurant.averageSalary) {
@@ -44,17 +46,18 @@ function solve() {
                 bestRestaurant.bestSalary = restaurantsData.bestSalary;
                 bestRestaurant.workers = restaurantsData.workers;
             }
-            console.log(restaurantsData);
         });
 
         let outputBestRestaurant = document.querySelector("#bestRestaurant p");
         outputBestRestaurant.textContent = `Name: ${bestRestaurant.name} Average Salary: ${bestRestaurant.averageSalary.toFixed(2)} Best Salary: ${bestRestaurant.bestSalary.toFixed(2)}`;
-    
+
         let outputWorkers = document.querySelector("#workers p");
 
-        Object.entries(bestRestaurant.workers).forEach(element => {
+        let sortedSalary = Object.entries(bestRestaurant.workers).sort((a, b) => b[1] - a[1]);
+        sortedSalary.forEach(element => {
             outputWorkers.textContent += `Name: ${element[0]} With Salary: ${element[1]} `
         })
+        outputWorkers.textContent = outputWorkers.textContent.trim();
     }
 }
 
