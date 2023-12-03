77eec95e168d05b7bf81789b298d63b2e045cb1f
Petko Todorov <p.todorov1015@gmail.com>
2023-12-03 13:03:31 +0200

    done

---
+++ DOM Manipulation and Events/Exercise/07. Distance-Converter/distanceConverter.js
@@ -0,0 +1,29 @@
+function attachEventsListeners() {
+    const units = {
+        "km": 1000,
+        "m": 1,
+        "cm": 0.01,
+        "mm": 0.001,
+        "mi": 1609.34,
+        "yrd": 0.9144,
+        "ft": 0.3048,
+        "in": 0.0254
+    }
+
+    const inputDistance = document.getElementById('inputDistance');
+    const inputUnits = document.getElementById('inputUnits');
+    const outputDistance = document.getElementById('outputDistance');
+    const outputUnits = document.getElementById('outputUnits');
+    const button = document.getElementById('convert')
+    button.addEventListener('click', result)
+
+
+    function result(e) {
+        let inputUnitsValue = units[inputUnits.value];
+        let inputDistanceValue = Number(inputDistance.value);
+        let outputUnitsValue = units[outputUnits.value]
+        let res = inputUnitsValue * inputDistanceValue / outputUnitsValue;
+        outputDistance.value = res;
+    }
+
+}
