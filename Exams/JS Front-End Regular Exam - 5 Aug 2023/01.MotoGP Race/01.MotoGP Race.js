4c0bb01be89acb6716c1951b799683bee156bda0
Petko Todorov <p.todorov1015@gmail.com>
2023-12-15 19:27:52 +0200

    remake

--- Exams/JS Front-End Regular Exam - 5 Aug 2023/01.MotoGP Race/01.MotoGP Race.js
+++ Exams/JS Front-End Regular Exam - 5 Aug 2023/01.MotoGP Race/01.MotoGP Race.js
@@ -1,63 +1,137 @@
-function motoGP(arr) {
-    let data = {};
-    let riderCount = Number(arr.shift());
+function solve(input) {
+    let riders = {
 
-    for (let index = 0; index < riderCount; index++) {
-        let [name, fuelCapacity, position] = arr.shift().split("|");
-        fuelCapacity = Number(fuelCapacity);
-        position = Number(position);
-        data[name] = {
-            fuelCapacity,
-            position
+    };
+
+    const number = Number(input.shift());
+
+    for (let index = 0; index < number; index++) {
+        const [rider, fuelCapacity, position] = input.shift().split('|');
+        riders[rider] = {
+            fuel: Number(fuelCapacity),
+            position: Number(position)
         }
     }
 
     while (true) {
-        let command = arr.shift();
-        if (command === "Finish") {
+        const command = input.shift();
+        if (command === 'Finish') {
             break;
         }
 
-        let [cmd, ...args] = command.split(" - ");
-        if (cmd === "StopForFuel") {
-            let [rider, minFuel, changedPosition] = args;
-            minFuel = Number(minFuel);
-            changedPosition = Number(changedPosition);
+        const [cmd, ...args] = command.split(' - ');
+        if (cmd === 'StopForFuel') {
 
-            if (data[rider].fuelCapacity <= minFuel) {
+            const [rider, minimumFuel, changedPosition] = args;
+            if (riders[rider].fuel < Number(minimumFuel)) {
+
+                riders[rider].position = changedPosition;
                 console.log(`${rider} stopped to refuel but lost his position, now he is ${changedPosition}.`);
-                data[rider].position = changedPosition;
-                data[rider].fuelCapacity = 100;
 
             } else {
                 console.log(`${rider} does not need to stop for fuel!`);
-                data[rider].fuelCapacity -= minFuel;
             }
 
-        } else if (cmd === "Overtaking") {
-            let [firstRider, secondRider] = args;
-            let firstPosition = data[firstRider].position;
-            let secondPosition = data[secondRider].position;
+        } else if (cmd === 'Overtaking') {
 
-            if (firstPosition > secondPosition) {
-                continue
+            const [rider1, rider2] = args;
+            const rider1Position = riders[rider1].position;
+            const rider2Position = riders[rider2].position;
+
+            if (rider1Position < rider2Position) {
+                riders[rider1].position = rider2Position;
+                riders[rider2].position = rider1Position;
+
+                console.log(`${rider1} overtook ${rider2}!`);
             }
-            data[firstRider].position = secondPosition;
-            data[secondRider].position = firstPosition;
 
-            console.log(`${firstRider} overtook ${secondRider}!`);
+        } else {
 
-        } else if (cmd === "EngineFail") {
-            let [rider, lapsLeft] = args;
-            delete data[rider];
+            const [rider, lapsLeft] = args;
+
+            delete riders[rider];
             console.log(`${rider} is out of the race because of a technical issue, ${lapsLeft} laps before the finish.`);
+
+
         }
     }
 
-    Object.entries(data).forEach(element => {
-        let key = element[0];
-        let value = element[1];
-        console.log(key);
-        console.log(`  Final position: ${value.position}`);
+    // for (const rider of Object.entries(riders)) {
+    //     console.log(rider[0]);
+    //     console.log(`  Final position: ${rider[1].position}`);
+    // }
+
+    Object.entries(riders).forEach(rider => {
+        console.log(rider[0]);
+        console.log(`  Final position: ${rider[1].position}`);
     });
 }
+
+
+
+
+// function motoGP(arr) {
+//     let data = {};
+//     let riderCount = Number(arr.shift());
+
+//     for (let index = 0; index < riderCount; index++) {
+//         let [name, fuelCapacity, position] = arr.shift().split("|");
+//         fuelCapacity = Number(fuelCapacity);
+//         position = Number(position);
+//         data[name] = {
+//             fuelCapacity,
+//             position
+//         }
+//     }
+
+//     while (true) {
+//         let command = arr.shift();
+//         if (command === "Finish") {
+//             break;
+//         }
+
+//         let [cmd, ...args] = command.split(" - ");
+//         if (cmd === "StopForFuel") {
+//             let [rider, minFuel, changedPosition] = args;
+//             minFuel = Number(minFuel);
+//             changedPosition = Number(changedPosition);
+
+//             if (data[rider].fuelCapacity <= minFuel) {
+//                 console.log(`${rider} stopped to refuel but lost his position, now he is ${changedPosition}.`);
+//                 data[rider].position = changedPosition;
+//                 data[rider].fuelCapacity = 100;
+
+//             } else {
+//                 console.log(`${rider} does not need to stop for fuel!`);
+//                 data[rider].fuelCapacity -= minFuel;
+//             }
+
+//         } else if (cmd === "Overtaking") {
+//             let [firstRider, secondRider] = args;
+//             let firstPosition = data[firstRider].position;
+//             let secondPosition = data[secondRider].position;
+
+//             if (firstPosition > secondPosition) {
+//                 continue
+//             }
+//             data[firstRider].position = secondPosition;
+//             data[secondRider].position = firstPosition;
+
+//             console.log(`${firstRider} overtook ${secondRider}!`);
+
+//         } else if (cmd === "EngineFail") {
+//             let [rider, lapsLeft] = args;
+//             delete data[rider];
+//             console.log(`${rider} is out of the race because of a technical issue, ${lapsLeft} laps before the finish.`);
+//         }
+//     }
+
+//     Object.entries(data).forEach(element => {
+//         let key = element[0];
+//         let value = element[1];
+//         console.log(key);
+//         console.log(`  Final position: ${value.position}`);
+//     });
+// }
+
+
